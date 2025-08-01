3c6079020a6f31f56201e045cffb62f50c6ad031
klorfmorf <169589138+klorfmorf@users.noreply.github.com>
2025-08-01 21:27:27 +0200

    `__ll_lshift_recomp` is actually implemented now.

--- src/game/unknown_symbols.cpp
+++ src/game/unknown_symbols.cpp
@@ -8,4 +8,3 @@ extern "C" void osPiReadIo_recomp(uint8_t* rdram, recomp_context* ctx) {}
 extern "C" void __osContRamWrite_recomp(uint8_t* rdram, recomp_context* ctx) {}
 extern "C" void __osViSwapContext_recomp(uint8_t* rdram, recomp_context* ctx) {}
 extern "C" void __osTimerInterrupt_recomp(uint8_t* rdram, recomp_context* ctx) {}
-extern "C" void __ll_lshift_recomp(uint8_t* rdram, recomp_context* ctx) {}
