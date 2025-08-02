7e332f652ecee17b774760fe2f3b8663fd66dabb
klorfmorf <169589138+klorfmorf@users.noreply.github.com>
2025-08-02 19:35:20 +0200

    Update symbols and add `rmonPrintf_recomp` stub.

--- src/game/unknown_symbols.cpp
+++ src/game/unknown_symbols.cpp
@@ -8,3 +8,4 @@ extern "C" void osPiReadIo_recomp(uint8_t* rdram, recomp_context* ctx) {}
 extern "C" void __osContRamWrite_recomp(uint8_t* rdram, recomp_context* ctx) {}
 extern "C" void __osViSwapContext_recomp(uint8_t* rdram, recomp_context* ctx) {}
 extern "C" void __osTimerInterrupt_recomp(uint8_t* rdram, recomp_context* ctx) {}
+extern "C" void rmonPrintf_recomp(uint8_t* rdram, recomp_context* ctx) {}
