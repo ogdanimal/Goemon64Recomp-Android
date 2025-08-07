e0e26a74f3b75f184fdd39eac0d1e57b557873f1
klorfmorf <169589138+klorfmorf@users.noreply.github.com>
2025-08-07 22:55:19 +0200

    Preparations for proper ROM decompression implementation.

--- include/goemon_game.h
+++ include/goemon_game.h
@@ -1,9 +1,14 @@
 #ifndef __GOEMON_GAME_H__
 #define __GOEMON_GAME_H__
 
+#include <cstdint>
+#include <span>
+#include <vector>
+
 namespace goemon64 {
     void quicksave_save();
     void quicksave_load();
+    std::vector<uint8_t> decompress_mnsg(std::span<const uint8_t> compressed_rom);
 };
 
 #endif
