9bd26dc8687748099e8436493825de56d72214a8
klorfmorf <169589138+klorfmorf@users.noreply.github.com>
2025-11-19 19:58:05 +0100

    Remove leftover batch scripts.

--- build_relwithdebinfo.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_C_COMPILER=clang-cl -DPATCHES_C_COMPILER="..\..\llvm\bin\clang" -DPATCHES_LD="..\..\llvm\bin\ld.lld" -DCMAKE_MAKE_PROGRAM=ninja -G Ninja -S . -B build/relwithdebinfo -DCMAKE_CXX_FLAGS="-Xclang -fexceptions -Xclang -fcxx-exceptions"
-cmake --build build/relwithdebinfo --config RelWithDebInfo --target Goemon64Recompiled -j
