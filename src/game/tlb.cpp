78a36090afafdfb523b7bc51feb24653a20a7e70
klorfmorf <169589138+klorfmorf@users.noreply.github.com>
2026-01-03 20:42:33 +0100

    Remove tlb.cpp as the stubs are handled by N64ModernRuntime now

--- src/game/tlb.cpp
+++
@@ -1,8 +0,0 @@
-#include "recomp.h"
-#include "librecomp/overlays.hpp"
-#include "librecomp/helpers.hpp"
-#include "ultramodern/ultramodern.hpp"
-
-extern "C" void osMapTLB_recomp(uint8_t* rdram, recomp_context* ctx) {}
-extern "C" void osUnmapTLB_recomp(uint8_t* rdram, recomp_context* ctx) {}
-extern "C" void osUnmapTLBAll_recomp(uint8_t* rdram, recomp_context* ctx) {}
