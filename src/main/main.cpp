14874bf06c4fb9310434a6fe46b3e146e4c4156d
klorfmorf <169589138+klorfmorf@users.noreply.github.com>
2026-01-31 16:22:14 +0100

    Bump version to v0.2.0

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -56,7 +56,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "0.1.0-dev";
+const std::string version_string = "0.2.0-dev";
 
 template<typename... Ts>
 void exit_error(const char* str, Ts ...args) {
